db64028f0011bfb7fc371cced36ae395597a570e
jasnoli <68044701+jasnoli@users.noreply.github.com>
2021-12-30 14:02:15 -0500

    Add output directory creation

--- synthpop.R
+++ synthpop.R
@@ -12,8 +12,8 @@ suppressPackageStartupMessages({
 options(dplyr.summarise.inform=F)
 
 rm(list=ls())
-graphics.off()
-cat("\014")
+# graphics.off()
+# cat("\014")
 set.seed(1033)
 
 synthpop_start_time <- Sys.time()
@@ -58,6 +58,17 @@ if("n_cores" %in% names(args)){
 
 ##### RUN SETTINGS #####
 
+Rscript_path <- "/Program Files/R/R-4.1.0/bin/Rscript.exe"
+
+julia_path <- "/Program Files/Julia-1.6.1/bin/julia.exe"
+
+gurobi_home <- '"C:\\Program Files\\gurobi910\\win64"'
+
+gurobi_license <- '"C:\\Program Files\\gurobi910\\gurobi.lic"'
+
+my_census_api_key <- "ac6cb3e106c860e52384fe71cf0407a13c25b96c"
+
+
 prepare_data_file <- "prepare_data.R"
 process_pums_file <- "process_pums.R"
 process_marg_file <- "process_marg.R"
@@ -67,26 +78,27 @@ sample_indvs_file <- "sample_indvs.R"
 select_synpop_file <- "select_synpop.jl"
 assign_spatial_file <- "assign_spatial.jl"
 
-hh_pool_file <- paste0("synthpop_output/hh_pool_", pumas, output_suffix, ".csv")
-indv_pool_file <- paste0("synthpop_output/indv_pool_", pumas, output_suffix, ".csv")
+hh_pool_file <- paste0("synthpop_output2/hh_pool_", pumas, output_suffix, ".csv")
+indv_pool_file <- paste0("synthpop_output3/indv_pool_", pumas, output_suffix, ".csv")
 syn_hhs_file <- paste0("synthpop_output/syn_hhs_", pumas, output_suffix, ".csv")
 syn_indvs_file <- paste0("synthpop_output/syn_indvs_", pumas, output_suffix, ".csv")
 syn_hhs_spatial_file <- paste0("synthpop_output/syn_hhs_spatial_", pumas, output_suffix, ".csv")
 syn_indvs_spatial_file <- paste0("synthpop_output/syn_indvs_spatial_", pumas, output_suffix, ".csv")
 
-Rscript_path <- "/Program Files/R/R-4.1.0/bin/Rscript.exe"
-
-julia_path <- "/Program Files/Julia-1.6.1/bin/julia.exe"
-
-gurobi_home <- '"C:\\Program Files\\gurobi910\\win64"'
-gurobi_license <- '"C:\\Program Files\\gurobi910\\gurobi.lic"'
 gurobi_logfile <- "synthpop_gurobi_log.txt"
-
-my_census_api_key <- "ac6cb3e106c860e52384fe71cf0407a13c25b96c"
 
 puma_tract_equiv_file <- "synthpop_data/2010_Census_Tract_to_2010_PUMA.csv"
 
 tract_block_equiv_file <- "synthpop_data/tract_block_equiv_2010.Rds"
+
+
+##### CREATE OUTPUT DIRECTORIES #####
+
+for(output_file in c(hh_pool_file, indv_pool_file, syn_hhs_file, syn_indvs_file, syn_hhs_spatial_file, syn_indvs_spatial_file)){
+  if(!dir.exists(dirname(output_file))) {
+    dir.create(dirname(output_file), recursive = T)
+  }
+}
 
 
 ##### DEFINE SYSTEM COMMANDS #####
